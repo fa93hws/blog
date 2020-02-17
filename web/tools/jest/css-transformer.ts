6a7368eb73f640e3ee26965d9d436d19284452f8
eric wang <eric@canva.com>
2020-02-18 00:12:27 +1100

    add comment

--- web/tools/jest/css-transformer.ts
+++ web/tools/jest/css-transformer.ts
@@ -1,4 +1,7 @@
 import * as idObj from 'identity-obj-proxy';
 
+// identity-obj-proxy return a es5 module
+// while it's used as es6 import
+
 // eslint-disable-next-line import/no-default-export
 export default idObj;
