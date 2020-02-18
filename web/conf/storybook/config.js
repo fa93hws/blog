43cebeca318527920313d0593a14d90d97294440
eric wang <eric@canva.com>
2020-02-19 00:03:03 +1100

    Globalize storybook addon and knobs

--- web/conf/storybook/config.js
+++
@@ -1,1 +0,0 @@
-import '../../src/pages/global.css';
---
+++ web/conf/storybook/config.ts
@@ -0,0 +1,5 @@
+import { withKnobs } from '@storybook/addon-knobs';
+import { addDecorator } from '@storybook/react';
+import '../../src/pages/global.css';
+
+addDecorator(withKnobs);
