8b76854c1ec92ecffcc70bf7aeda84afda229b53
eric wang <eric@canva.com>
2020-04-04 20:39:04 +1100

    Change to use typography

--- web/src/components/nav/banner/banner.tsx
+++ web/src/components/nav/banner/banner.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Text } from '../../typography/typography';
 import styles from './banner.css';
 
 export const SourceCodeBanner = ({
@@ -7,6 +8,8 @@ export const SourceCodeBanner = ({
   sourceCodeUrl: string;
 }) => (
   <div className={styles.banner}>
-    <a href={sourceCodeUrl}>源代码</a>
+    <a href={sourceCodeUrl}>
+      <Text.Micro>源代码</Text.Micro>
+    </a>
   </div>
 );
