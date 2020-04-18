fdc1420cf907836d522b1c7ee4c8e5017adb56c0
eric wang <eric@canva.com>
2020-04-18 22:00:15 +1000

    fix spacing in icons

--- web/src/components/buttons/ripple/tests/ripple.tests.tsx
+++ web/src/components/buttons/ripple/tests/ripple.tests.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { mount } from 'enzyme';
 import { Internal, withRipple, RipplableProps } from '../ripple';
 import { RippleStore } from '../ripple-store';
 
@@ -25,5 +26,16 @@ describe('Ripple', () => {
     store.setRipple({ top: 2, left: 4 });
     const RippleComponent = withRipple(Container);
     expect(<RippleComponent store={store} />).toMatchRenderedSnapshot();
+  });
+
+  it('triggers ripple when mouse down', () => {
+    const store = new RippleStore();
+    const RippleComponent = withRipple(Container);
+    const wrapper = mount(<RippleComponent store={store} />);
+    wrapper.find('button').simulate('mouseDown', { clientX: 10, clientY: 20 });
+    expect(store.ripplePosition).toEqual({
+      left: 10,
+      top: 20,
+    });
   });
 });
