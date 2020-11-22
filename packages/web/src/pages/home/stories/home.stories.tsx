8d362f3c5c5ca2ad108fc5116e6f7699b066561c
eric wang <eric@canva.com>
2020-11-22 14:41:26 +1100

    chore: Remove old web content

--- packages/web/src/pages/home/stories/home.stories.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { storiesOf } from '@storybook/react';
-import { fakeBlogList } from 'services/blog-list/fake/fake-blog-list';
-import StoryRouter from 'storybook-react-router';
-import { HomePage } from '../home';
-
-storiesOf('pages.home', module)
-  .addDecorator(StoryRouter())
-  .add('home page', () => <HomePage blogList={fakeBlogList} />);
