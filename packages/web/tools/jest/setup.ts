1b2ae30270ef1dcfdbe88cafc3272c3971a96f99
eric wang <eric@canva.com>
2020-11-22 15:50:23 +1100

    fix types

--- packages/web/tools/jest/setup.ts
+++ packages/web/tools/jest/setup.ts
@@ -1,9 +1,4 @@
-import { configure } from 'enzyme';
-import * as Adapter from 'enzyme-adapter-react-16';
 import { toMatchRenderedSnapshot } from './rendered-snapshot';
-import 'mobx-react/batchingOptOut';
-
-configure({ adapter: new Adapter() });
 
 expect.extend({
   toMatchRenderedSnapshot,
