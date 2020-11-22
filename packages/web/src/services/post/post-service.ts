8963ac1107a14f342e8f9eba20a07e1926168e7e
eric wang <eric@canva.com>
2020-11-22 23:29:18 +1100

    chore: Add tests to services in web;

--- packages/web/src/services/post/post-service.ts
+++ packages/web/src/services/post/post-service.ts
@@ -2,14 +2,13 @@ import { HttpEngine } from '@services/http-engine/http-engine';
 import { PostProto } from '@fa93hws-blog/protos';
 import { Result } from '@badrap/result';
 
-type Summary = PostProto.ISummary;
 type SummaryList = PostProto.ISummrayList;
 
 export class PostService {
   constructor(private readonly httpEngine: HttpEngine) {}
 
   async fetchList(): Promise<Result<SummaryList>> {
-    const result = await this.httpEngine.get<PostProto.ISummrayList>('posts');
+    const result = await this.httpEngine.get<SummaryList>('posts');
     if (result.isErr) {
       return Result.err(result.error);
     }
