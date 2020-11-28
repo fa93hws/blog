4bc63f016504517f0543d359763dc27945ce6f7e
eric wang <eric@canva.com>
2020-11-28 19:12:16 +1100

    change story name

--- packages/web/src/components/sidebar/stories/sidebar.stories.tsx
+++ packages/web/src/components/sidebar/stories/sidebar.stories.tsx
@@ -2,4 +2,4 @@ import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import { SideBar } from '../sidebar';
 
-storiesOf('components.header', module).add('Header', () => <SideBar />);
+storiesOf('components.sidebar', module).add('Header', () => <SideBar />);
