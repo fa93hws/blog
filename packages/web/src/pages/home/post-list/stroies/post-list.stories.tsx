74afa825acec05313be8c68cf02eb7662c99ce1c
eric wang <eric@canva.com>
2020-11-29 12:55:53 +1100

    feat: Add loading to home page

--- packages/web/src/pages/home/post-list/stroies/post-list.stories.tsx
+++ packages/web/src/pages/home/post-list/stroies/post-list.stories.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import { fakePostList } from '@services/post/fake/fake-post-service';
-import { PostList } from '../post-list';
+import { PostList, PostListPlaceholder } from '../post-list';
 
-storiesOf('components.footer', module).add('Footer', () => {
-  return <PostList posts={fakePostList.list ?? []} />;
-});
+storiesOf('pages.home.post-list', module)
+  .add('PostList', () => {
+    return <PostList posts={fakePostList.list ?? []} />;
+  })
+  .add('PostList(Loading)', () => {
+    return <PostListPlaceholder />;
+  });
