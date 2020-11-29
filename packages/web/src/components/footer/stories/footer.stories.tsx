874cb6192afa114b80a40c80220e037c75138026
eric wang <eric@canva.com>
2020-11-29 15:30:53 +1100

    chore: Allow some stories to be skipped on percy

--- packages/web/src/components/footer/stories/footer.stories.tsx
+++ packages/web/src/components/footer/stories/footer.stories.tsx
@@ -2,4 +2,6 @@ import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Footer } from '../footer';
 
-storiesOf('components.footer', module).add('Footer', () => <Footer />);
+storiesOf('components.footer', module)
+  .addParameters({ percy: { skip: true } })
+  .add('Footer', () => <Footer />);
