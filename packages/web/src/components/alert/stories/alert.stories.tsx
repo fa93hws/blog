2a45ff2498fd73d7e7798a6d6d97d7868a8c2397
Eric Wang <wjun0912@gmail.com>
2020-11-29 15:58:37 +1100

    chore: Allow some stories to be skipped on percy (#112)

--- packages/web/src/components/alert/stories/alert.stories.tsx
+++ packages/web/src/components/alert/stories/alert.stories.tsx
@@ -4,25 +4,27 @@ import { Color } from '@material-ui/lab/Alert';
 import { text, select } from '@storybook/addon-knobs';
 import { createAlert } from '../alert';
 
-storiesOf('components.alert', module).add('Alert', () => {
-  const { Component, showGlobalMsg } = createAlert();
-  const msg = text('global message', '顾茗茗');
-  const severity: Color = select(
-    'severity',
-    {
-      error: 'error',
-      success: 'success',
-      warning: 'warning',
-      info: 'info',
-    },
-    'error',
-  );
-  const handleClick = () =>
-    showGlobalMsg(severity, msg, { key: Math.random().toString() });
-  return (
-    <div>
-      <Component />
-      <button onClick={handleClick}>点我啊！杂种！</button>
-    </div>
-  );
-});
+storiesOf('components.alert', module)
+  .addParameters({ percy: { skip: true } })
+  .add('Alert', () => {
+    const { Component, showGlobalMsg } = createAlert();
+    const msg = text('global message', '顾茗茗');
+    const severity: Color = select(
+      'severity',
+      {
+        error: 'error',
+        success: 'success',
+        warning: 'warning',
+        info: 'info',
+      },
+      'error',
+    );
+    const handleClick = () =>
+      showGlobalMsg(severity, msg, { key: Math.random().toString() });
+    return (
+      <div>
+        <Component />
+        <button onClick={handleClick}>点我啊！杂种！</button>
+      </div>
+    );
+  });
