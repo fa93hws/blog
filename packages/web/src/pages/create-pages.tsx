d9bf6f27091f084299a948043238f42faca8aca5
Eric Wang <wjun0912@gmail.com>
2020-12-06 13:37:49 +1100

    refactor: move post service to global context (#119)

--- packages/web/src/pages/create-pages.tsx
+++ packages/web/src/pages/create-pages.tsx
@@ -1,14 +1,19 @@
 import * as React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { createHome } from '@pages/home/home';
+import type { ShowFn } from '@components/alert/alert';
 import type { GlobalContext } from '@utils/global-context';
 import { createService } from './create-service';
 import { createPost } from './post/post';
 
-export function createPages(globalContext: GlobalContext) {
+export function createPages(showGlobalMsg: ShowFn) {
   const { postService } = createService();
-  const HomePage = createHome(postService, globalContext);
-  const PostPage = createPost(postService, globalContext);
+  const globalContext: GlobalContext = {
+    postService,
+    showGlobalMsg,
+  };
+  const HomePage = createHome(globalContext);
+  const PostPage = createPost(globalContext);
 
   return React.memo(() => (
     <Switch>
